125719c9591e18d308ba9b3420af762f2f06b96f
danielgomez3 <danielgomez3@verizon.net>
2024-07-09 10:10:40 -0400

    desktop

--- laptop.nix
+++ laptop.nix
@@ -16,7 +16,7 @@
   networking.hostName = host; # Define your hostname.
   services = {
     syncthing = {
-      # guiAddress = "127.0.0.1:8383";
+      guiAddress = "127.0.0.1:8383";
       settings = {
         devices = {
           "desktop" = { id = "23GQV6A-ROUBBEV-N2S53WL-R6WJXFT-G2UKAXW-7D7C44A-USAGYXV-GW6WAQ2"; autoAcceptFolders = true;};
