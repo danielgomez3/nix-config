5c6110d71a758acdd8cd3f53b90d8a35bdfc51d4
danielgomez3 <danielgomez3@verizon.net>
2024-07-17 17:49:23 -0400

    desktop

--- flakes/workspace.nix
+++ flakes/workspace.nix
@@ -321,6 +321,14 @@ in
           enableZshIntegration = true;
           enableFishIntegration = true;
         };
+        vscode = {
+          enable = true;
+          extensions = with pkgs.vscode-extensions; [
+            dracula-theme.theme-dracula
+            vscodevim.vim
+            yzhang.markdown-all-in-one
+          ];
+        };
         emacs = {
           enable = true;
           # package = emacsPackages.doom;
