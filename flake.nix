43c4ee1202f7046c70043b3a3c60ab1a8c793f0a
danielgomez3 <danielgomez3@verizon.net>
2024-07-24 00:21:58 -0400

    working, about to do weird hardware config

--- flake.nix
+++ flake.nix
@@ -6,58 +6,67 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-  };
-
-  outputs = inputs@{ self, nixpkgs, ... }: {
-    nixosConfigurations = {
-
-      desktop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-          username = "daniel";
-          host = "desktop";
-          };
-        modules = [
-          ./devices/desktop.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
-
-      laptop = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-          username = "daniel";
-          host = "laptop";
-        };
-        modules = [
-          ./devices/laptop.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
-
-      server = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
-          username = "danielgomez3";
-          host = "";
-        };
-        modules = [
-          ./devices/server.nix
-          inputs.home-manager.nixosModules.default
-        ];
-      };
-
-      customIso = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          ./scripts/installer/installer.nix
-        ];
-      };
-
-
+    deploy-flake = {
+      url = "github:boinkor-net/deploy-flake";
+      # The following are optional, but probably a good idea if you have these inputs:
+      inputs.nixpkgs.follows = "nixpkgs";
+      # inputs.rust-overlay.follows = "rust-overlay";
     };
   };
+
+  outputs = inputs@{ self, nixpkgs, deploy-flake, ... }: 
+    let 
+      system = "x86_64-linux";
+    in {
+      nixosConfigurations = {
+
+        apps.deploy-flake = deploy-flake.apps.deploy-flake.${system};
+
+        desktop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            username = "daniel";
+            host = "desktop";
+            };
+          modules = [
+            ./devices/desktop.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+
+        laptop = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            username = "daniel";
+            host = "laptop";
+          };
+          modules = [
+            ./devices/laptop.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+
+        server = nixpkgs.lib.nixosSystem {
+          inherit system;
+          specialArgs = {
+            inherit inputs;
+            username = "danielgomez3";
+            host = "server";
+          };
+          modules = [
+            ./devices/server.nix
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+
+        customIso = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./scripts/installer/installer.nix
+          ];
+        };
+      };
+    };
 }
