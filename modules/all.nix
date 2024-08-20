e8f7a29f5a61502e46a1d3d2662981419a535b3a
danielgomez3 <danielgomez3@verizon.net>
2024-08-20 12:43:39 -0400

    server

--- modules/all.nix
+++ modules/all.nix
@@ -85,6 +85,24 @@ in
           KbdInteractiveAuthentication = false;
           # PermitRootLogin = "yes";        # Allow root login with password
         };
+        knownHosts = {
+          "desktop" = {
+            hostNames = [ "desktop" ];
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM9OcZ6CO1lDXOMQQawee8Fh6iydI8I+SXMdD9GESq8v";
+          };
+          "server" = {
+            hostNames = [ "server" ];
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHdnOQw9c23oUEIBdZFrKb/r4lHIKLZ9Dz11Un0erVsj";
+          };
+          "laptop" = {
+            hostNames = [ "laptop" ];
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQ4W1AIoMxiKJQXOwJlkJkwZ0pMOe/akO86duVI/NWG";
+          };
+          "deploy-server" = {
+            hostNames = [ "server" ];
+            publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIEA/kfm1TYsOaPnzbLYnWixnjHSYWgYcS82z/xQGKgwb";
+          };
+        };
       };
       syncthing = {
         enable = true;
@@ -296,15 +314,13 @@ in
                HostName 192.168.12.149 
                User danielgomez3
 
-            Host server-deploy
+            Host deploy
                HostName 192.168.12.149 
                User deploy
 
             Host desktop
                HostName 192.168.12.182
-               User daniel
-        
-
+               User daniel     
           '';
           };
         };
