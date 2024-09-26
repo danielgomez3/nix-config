6a743d07d25f18b40c8e9ad431c59ba0eef1eeb1
danielgomez3 <danielgomez3@verizon.net>
2024-09-26 14:06:05 -0400

    server

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -11,20 +11,8 @@
       home = {
         stateVersion = "24.05";
       };
-      programs = {
-        # FIXME: Add laptop. Also, this has to be changed with all.nix's version. Pretty stupid.
-        ssh = {
-          extraConfig = ''
-            Host server
-               HostName 192.168.12.149 
-               User danielgomez3
-
-            Host desktop
-               HostName 192.168.12.182
-               User daniel
-          '';
-          };
-        };
+      # programs = {
+      #   };
       };
     };
 
@@ -48,33 +36,6 @@
       openFirewall = true;
       user = "${username}";
       # dataDir = "/home/${username}/plex";
-    };
-
-    openssh = {
-      enable = true;
-      settings = {
-        PasswordAuthentication = false;
-        KbdInteractiveAuthentication = false;
-        # PermitRootLogin = "yes";        # Allow root login with password
-      };
-      knownHosts = {
-        "desktop" = {
-          hostNames = [ "desktop" ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM9OcZ6CO1lDXOMQQawee8Fh6iydI8I+SXMdD9GESq8v";
-        };
-        "server" = {
-          hostNames = [ "server" ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHdnOQw9c23oUEIBdZFrKb/r4lHIKLZ9Dz11Un0erVsj";
-        };
-        "laptop" = {
-          hostNames = [ "laptop" ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGQ4W1AIoMxiKJQXOwJlkJkwZ0pMOe/akO86duVI/NWG";
-        };
-        "deploy" = {
-          hostNames = [ "root" ];
-          publicKey = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGYrgc8D5QnHXMZT+npgXshrn4LSfDy8qlwHF53m/dvz";
-        };
-      };
     };
 
     syncthing = {
