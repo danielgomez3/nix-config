a6374de33863b55fcc367a9ee7524125671c1cc3
Daniel Gomez <danielgomezcoder@gmail.com>
2024-10-29 11:34:48 -0400

    adding laptop disko

--- modules/all.nix
+++ modules/all.nix
@@ -139,15 +139,15 @@ in
     services = { 
       openssh = {
         enable = true;
-        hostKeys = [
-          {
-            # XXX: No longer needed? Just generate a root user key and copy it over to /etc/ssh. Or what is done here: maybe you could set the path to /root/.ssh/id_ed25519 then copy that over.
-            comment = "to be copied over to /etc/ssh/";
-            path = "/etc/ssh/ssh_host_ed25519_key";
-            # path = "/root/.ssh/id_ed25519";
-            type = "ed25519";
-          }
-        ];
+        # hostKeys = [
+        #   {
+        #     # XXX: No longer needed? Just generate a root user key and copy it over to /etc/ssh. Or what is done here: maybe you could set the path to /root/.ssh/id_ed25519 then copy that over.
+        #     comment = "to be copied over to /etc/ssh/";
+        #     path = "/etc/ssh/ssh_host_ed25519_key";
+        #     # path = "/root/.ssh/id_ed25519";
+        #     type = "ed25519";
+        #   }
+        # ];
         settings = {
           PasswordAuthentication = false;
           KbdInteractiveAuthentication = false;
