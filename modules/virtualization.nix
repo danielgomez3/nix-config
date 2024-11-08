fa761cec47978e9a0619248d6558b40f513bc7d5
Daniel Gomez <danielgomezcoder@gmail.com>
2024-11-08 15:08:56 -0500

    added virtual

--- modules/virtualization.nix
+++ modules/virtualization.nix
@@ -1,1 +1,32 @@
-{config, pkgs, ...}:{}
+{ config, lib, username, ...}:
+let
+  cfg = config.services.virtualization;
+in
+  {
+    options.services.virtualization = {
+      enable = lib.mkEnableOption "virtualization service";  
+    };
+
+    config = lib.mkIf cfg.enable {
+
+      # users.extraGroups.vboxusers.members = [ "daniel" ];
+      users.users.${username}.extraGroups = [ "docker" ];
+
+      virtualisation = {
+        docker = {
+          enable = true;
+          rootless = {
+            enable = true;
+            setSocketVariable = true;
+          };
+        }; 
+        # virtualbox = {
+        #   host = {
+        #     enable = true;
+        #     enableExtensionPack = true;
+        #   };
+        # };
+      };
+    
+    };
+  }
