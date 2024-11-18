5ec7fa81a4913a4a4772447e9ef58de80935df93
Daniel Gomez <danielgomezcoder@gmail.com>
2024-11-18 09:33:02 -0500

    CAUTION untested changes, possibly broken

--- modules/gui.nix
+++ modules/gui.nix
@@ -601,18 +601,6 @@ in
               no_fade_in = false;
             };
 
-            input-field = [
-              {
-                size = "200, 50";
-                position = "0, -80";
-                monitor = "";
-                dots_center = true;
-                fade_on_empty = false;
-                outline_thickness = 5;
-                # placeholder_text = '\'Password...'\';
-                shadow_passes = 2;
-              }
-            ];
           };
         };
       };
