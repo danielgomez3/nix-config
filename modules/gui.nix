e637cfbdd4f340335e5125676e3cc43a38c2c409
Daniel Gomez <danielgomezcoder@gmail.com>
2024-11-18 09:48:02 -0500

    CAUTION untested changes, possibly broken

--- modules/gui.nix
+++ modules/gui.nix
@@ -590,16 +590,17 @@ in
         };
         hyprlock = {
           enable = true;
-          settings = {
-            general = {
-              disable_loading_bar = true;
-              grace = 10;
-              hide_cursor = true;
-              no_fade_in = false;
-            };
+          # settings = {
+          #   general = {
+          #     disable_loading_bar = true;
+          #     grace = 10;
+          #     hide_cursor = true;
+          #     no_fade_in = false;
+          #   };
 
-          };
+          # };
         };
+
       };
     };
   };
