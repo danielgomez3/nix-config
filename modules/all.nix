53dc951a3f6a29ce71643eb62cf589d9c67a3a19
Daniel Gomez <danielgomezcoder@gmail.com>
2024-11-20 09:16:26 -0500

    CAUTION untested changes, possibly broken

--- modules/all.nix
+++ modules/all.nix
@@ -86,34 +86,6 @@ in
     # https://wiki.nixos.org/wiki/Syncthing#tips
     # Don't create default ~/Sync folder
     systemd.services = {
-    # create a oneshot job to authenticate to Tailscale
-      tailscale-autoconnect = {
-        description = "Automatic connection to Tailscale";
-
-        # make sure tailscale is running before trying to connect to tailscale
-        after = [ "network-pre.target" "tailscale.service" ];
-        wants = [ "network-pre.target" "tailscale.service" ];
-        wantedBy = [ "multi-user.target" ];
-
-        # set this service as a oneshot job
-        serviceConfig.Type = "oneshot";
-
-        # have the job run this shell script
-        # FIXME: using 'cat' for the password may be impure and may cause side effects when deploying using NixOs-Anywhere... Investigate!
-        script = with pkgs; ''
-          # wait for tailscaled to settle
-          sleep 2
-
-          # check if we are already authenticated to tailscale
-          status="$(${tailscale}/bin/tailscale status -json | ${jq}/bin/jq -r .BackendState)"
-          if [ $status = "Running" ]; then # if so, then do nothing
-            exit 0
-          fi
-
-          # otherwise authenticate with tailscale
-          ${tailscale}/bin/tailscale up -authkey $(cat ${config.sops.secrets."tailscale/${user}".path})
-        '';
-      };
       syncthing.environment.STNODEFAULTFOLDER = "true";  
     };
 
