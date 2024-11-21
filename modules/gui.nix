e296aaafa34dbb2fc53dbf44882e88988d185e36
Daniel Gomez <danielgomezcoder@gmail.com>
2024-11-20 23:34:11 -0500

    CAUTION untested changes, possibly broken

--- modules/gui.nix
+++ modules/gui.nix
@@ -384,14 +384,13 @@ in
             "plexamp"
             "firefox"
             "pavucontrol"
-            "kitty"
           ];
           windowrule = [
-            "workspace 1, firefox"
-            "workspace 2, Slack"
-            "workspace 2, org.kde.kdeconnect.sms"
-            "workspace 10, Plexamp"
-            "workspace 10, pavucontrol"
+            "workspace 1 silent, firefox"
+            "workspace 2 silent, Slack"
+            "workspace 2 silent, org.kde.kdeconnect.sms"
+            "workspace 10 silent, Plexamp"
+            "workspace 10 silent, pavucontrol"
           ];
 
           # listener = [
@@ -409,6 +408,39 @@ in
       };
 
       programs = {
+
+        waybar = {
+          enable = true;
+          settings = [
+            {
+              mainBar = {
+                layer = "top";
+                position = "top";
+                height = 30;
+                output = [
+                  "eDP-1"
+                  "HDMI-A-1"
+                ];
+                modules-left = [ "sway/workspaces" "sway/mode" "wlr/taskbar" ];
+                modules-center = [ "sway/window" "custom/hello-from-waybar" ];
+                modules-right = [ "mpd" "custom/mymodule#with-css-id" "temperature" ];
+
+                "sway/workspaces" = {
+                  disable-scroll = true;
+                  all-outputs = true;
+                };
+                "custom/hello-from-waybar" = {
+                  format = "hello {}";
+                  max-length = 40;
+                  interval = "once";
+                  exec = pkgs.writeShellScript "hello-from-waybar" ''
+                    echo "from within waybar"
+                  '';
+                };
+              };
+            }
+          ];
+        };
 
         swaylock = {
           enable = false;
