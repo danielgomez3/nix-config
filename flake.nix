4d4f4d10ab200438e4c2b323ee36780ce8e22b3e
danielgomez3 <danielgomezcoder@gmail.com>
2024-12-03 12:08:32 -0500

    CAUTION untested changes, possibly broken

--- flake.nix
+++ flake.nix
@@ -3,8 +3,8 @@
   description = "danielgomez3's NixOS configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    # nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+    # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     # wezterm-flake.url = "github:wez/wezterm/main?dir=nix";
