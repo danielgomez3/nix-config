82cbacf75487db9bd801df619adc32ed58d46b53
danielgomez3 <danielgomezcoder@gmail.com>
2024-12-03 13:24:42 -0500

    CAUTION untested changes, possibly broken

--- modules/all.nix
+++ modules/all.nix
@@ -82,38 +82,38 @@ in
         # };    
       };
     };
-    stylix = {
-      enable = false;
-      image = ./additional/wallpapers/cotton-candy.png;
-      # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
-      base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox.yaml";
-      # cursor = { 
-      #   package = pkgs.bibata-cursors; 
-      #   name = "Bibata-Modern-Ice";
-      #   # size = 50;
-      # };
-      # targets.nixvim.enable = false;
-      # fonts = {
-      #   monospace = {
-      #     package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
-      #     name = "JetBrainsMono Nerd Font Mono";
-      #   };
-      #   sansSerif = {
-      #     package = pkgs.dejavu_fonts;
-      #     name = "DejaVu Sans";
-      #   };
-      #   serif = {
-      #     package = pkgs.dejavu_fonts;
-      #     name = "DejaVu Serif";
-      #   };
-      # };
-      targets = {
-      #   helix.enable = true;
-      #   sway.enable = true;
-      #   swaylock.enable = true;
-      #   wezterm.enable = true;
-      };
-    };
+    # stylix = {
+    #   enable = false;
+    #   image = ./additional/wallpapers/cotton-candy.png;
+    #   # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+    #   base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox.yaml";
+    #   # cursor = { 
+    #   #   package = pkgs.bibata-cursors; 
+    #   #   name = "Bibata-Modern-Ice";
+    #   #   # size = 50;
+    #   # };
+    #   # targets.nixvim.enable = false;
+    #   # fonts = {
+    #   #   monospace = {
+    #   #     package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+    #   #     name = "JetBrainsMono Nerd Font Mono";
+    #   #   };
+    #   #   sansSerif = {
+    #   #     package = pkgs.dejavu_fonts;
+    #   #     name = "DejaVu Sans";
+    #   #   };
+    #   #   serif = {
+    #   #     package = pkgs.dejavu_fonts;
+    #   #     name = "DejaVu Serif";
+    #   #   };
+    #   # };
+    #   targets = {
+    #   #   helix.enable = true;
+    #   #   sway.enable = true;
+    #   #   swaylock.enable = true;
+    #   #   wezterm.enable = true;
+    #   };
+    # };
 
     # https://wiki.nixos.org/wiki/Syncthing#tips
     # Don't create default ~/Sync folder
