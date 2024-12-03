a2f8e255286fcd08986e32bd96057e7abd8f8ab5
danielgomez3 <danielgomezcoder@gmail.com>
2024-12-03 11:52:48 -0500

    CAUTION untested changes, possibly broken

--- modules/all.nix
+++ modules/all.nix
@@ -86,6 +86,32 @@ in
       enable = true;
       image = ./additional/wallpapers/cotton-candy.png;
       base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-macchiato.yaml";
+      # cursor = { 
+      #   package = pkgs.bibata-cursors; 
+      #   name = "Bibata-Modern-Ice";
+      #   # size = 50;
+      # };
+      # targets.nixvim.enable = false;
+      # fonts = {
+      #   monospace = {
+      #     package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
+      #     name = "JetBrainsMono Nerd Font Mono";
+      #   };
+      #   sansSerif = {
+      #     package = pkgs.dejavu_fonts;
+      #     name = "DejaVu Sans";
+      #   };
+      #   serif = {
+      #     package = pkgs.dejavu_fonts;
+      #     name = "DejaVu Serif";
+      #   };
+      # };
+      # targets = {
+      #   helix.enable = true;
+      #   sway.enable = true;
+      #   swaylock.enable = true;
+      #   wezterm.enable = true;
+      # };
     };
 
     # https://wiki.nixos.org/wiki/Syncthing#tips
