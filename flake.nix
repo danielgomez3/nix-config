fa6722cc68d8352f429b3bf4e46a5c86e3076d17
danielgomez3 <danielgomezcoder@gmail.com>
2024-12-05 12:53:51 -0500

    CAUTION untested changes, possibly broken

--- flake.nix
+++ flake.nix
@@ -38,12 +38,6 @@
       meta = {
         nixpkgs = import nixpkgs {
           system = system;
-          # config.permittedInsecurePackages = [  # FIXME: Remove this and figure out what depends!
-          #   "dotnet-core-combined"
-          #   "dotnet-sdk-6.0.428"
-          #   "dotnet-sdk-wrapped-6.0.428"
-          #   "dotnet-sdk-wrapped-6.0.428"
-          # ];
         };
         specialArgs = {
           inherit inputs;
