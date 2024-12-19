e071a515cc6fb06cad2b00b39027f795b625bcea
danielgomez3 <danielgomezcoder@gmail.com>
2024-12-19 13:26:51 -0500

    CAUTION untested changes, possibly broken

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -63,17 +63,17 @@ in
     vaultwarden = {
       enable = true;
     };
-    nginx = {
-      enable = true;
-      recommendedGzipSettings = true;
-      virtualHosts."server.tail1b372c.ts.net" = {
-        enableACME = false;  # Disable Let's Encrypt
-        forceSSL = false;    # Skip HTTPS enforcement (Tailscale already encrypts traffic)
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8050";
-        };
-      };
-    };
+    # nginx = {
+    #   enable = true;
+    #   recommendedGzipSettings = true;
+    #   virtualHosts."server.tail1b372c.ts.net" = {
+    #     enableACME = false;  # Disable Let's Encrypt
+    #     forceSSL = false;    # Skip HTTPS enforcement (Tailscale already encrypts traffic)
+    #     locations."/" = {
+    #       proxyPass = "http://127.0.0.1:8050";  # Forward to Vaultwarden
+    #     };
+    #   };
+    # };
     ddclient = {
       domains = [ 
           "server.danielgomezcoder.org"
