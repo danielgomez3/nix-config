296b5942373e42ac8bf41e1ef17b084c01c79814
danielgomez3 <danielgomezcoder@gmail.com>
2024-12-19 12:11:26 -0500

    CAUTION untested changes, possibly broken

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -56,6 +56,9 @@ in
       user = "${username}";
       # dataDir = "/home/${username}/plex";
     };
+    vaultwarden = {
+      enable = true;
+    };
     ddclient = {
       domains = [ 
           "server.danielgomezcoder.org"
