ec9e2acd9221ec0a89e4945d28d00c5910cd6633
danielgomez3 <danielgomezcoder@gmail.com>
2024-12-23 10:44:18 -0500

    CAUTION untested changes, possibly broken

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -80,7 +80,7 @@ in
         gui = {
           user = "boo";
           # password = "naruto88";
-          password = config.sops.secrets.syncthing.gui_password.path;
+          password = "${toString config.sops.secrets.syncthing.gui_password.path}";
         };
         folders = {
           "Documents" = {         # Name of folder in Syncthing, also the folder ID
