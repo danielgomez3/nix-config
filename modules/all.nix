8cba56cfc6c5939bcc821604bbcea20a0520c5ad
danielgomez3 <danielgomezcoder@gmail.com>
2024-12-23 10:37:50 -0500

    CAUTION untested changes, possibly broken

--- modules/all.nix
+++ modules/all.nix
@@ -200,7 +200,7 @@ in
         };
       };
       syncthing = {
-        enable = false;
+        enable = true;
         user = username;
         key = config.sops.secrets."syncthing/${name}/key_pem".path;
         cert = config.sops.secrets."syncthing/${name}/cert_pem".path;
