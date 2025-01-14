879d48946d4329ffccd1f3dcc409852032c8fc9f
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-14 14:39:48 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/homeManagerModules/programs/wezterm.nix
+++ modules/homeManagerModules/programs/wezterm.nix
@@ -4,8 +4,9 @@
     # package = inputs.wezterm.packages.${pkgs.system}.default;
     extraConfig = ''
       return {
-        front_end = "WebGpu",  -- HACKME: see if can remove in future NixOS version or patch
-        enable_tab_bar = false,
+        -- HACKME: see if can remove in future NixOS version or patch
+        front_end = "WebGpu",  
+        hide_tab_bar_if_only_one_tab = true
       }
     '';
   };
