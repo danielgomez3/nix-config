ceaf786f54547cb0a2ae9dd5e10326518e021f84
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-14 12:53:46 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/nixosModules/features/suspend-and-hibernate.nix
+++ modules/nixosModules/features/suspend-and-hibernate.nix
@@ -3,7 +3,6 @@ let
   username = config.myVars.username;
 in
 {
-  home-manager.users.${username}.programs.swaylock.enable = true;
 
   systemd.sleep.extraConfig = ''
     AllowSuspend=yes
@@ -12,26 +11,29 @@ in
     AllowSuspendThenHibernate=yes
   '';
 
-  services = {
-    hypridle = {
-      enable = true;
-      settings = {
-        general = {
-          after_sleep_cmd = "hyprctl dispatch dpms on";
-          ignore_dbus_inhibit = false;
-          lock_cmd = "swaylock";
+  home-manager.users.${username} = {
+    programs.swaylock.enable = true;
+    services = {
+      hypridle = {
+        enable = true;
+        settings = {
+          general = {
+            after_sleep_cmd = "hyprctl dispatch dpms on";
+            ignore_dbus_inhibit = false;
+            lock_cmd = "swaylock";
+          };
+          listener = [
+            {
+              timeout = 300;
+              on-timeout = "swaylock";
+            }
+            {
+              timeout = 390;
+              on-timeout = "systemctl suspend";
+              on-resume = "hyprctl dispatch dpms on";
+            }
+          ];
         };
-        listener = [
-          {
-            timeout = 300;
-            on-timeout = "swaylock";
-          }
-          {
-            timeout = 390;
-            on-timeout = "systemctl suspend";
-            on-resume = "hyprctl dispatch dpms on";
-          }
-        ];
       };
     };
   };
