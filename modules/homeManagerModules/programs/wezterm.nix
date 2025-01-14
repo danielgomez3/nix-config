b909ca499ce2bb0e12b8df31baa08f0da6534513
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-14 14:40:28 -0500

    Successful apply! No commit message given.

--- modules/homeManagerModules/programs/wezterm.nix
+++ modules/homeManagerModules/programs/wezterm.nix
@@ -6,7 +6,7 @@
       return {
         -- HACKME: see if can remove in future NixOS version or patch
         front_end = "WebGpu",  
-        hide_tab_bar_if_only_one_tab = true
+        hide_tab_bar_if_only_one_tab = true,
       }
     '';
   };
