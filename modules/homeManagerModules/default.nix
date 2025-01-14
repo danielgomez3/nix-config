6707bf0ac6f27143fdb821234383df36ea21f54d
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-14 10:08:25 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/homeManagerModules/default.nix
+++ modules/homeManagerModules/default.nix
@@ -47,38 +47,19 @@
   #   (myLib.filesIn ./bundles);
 
 in {
-  home-manager = {
-    backupFileExtension = "hm-backup";
-    extraSpecialArgs = { inherit inputs; };
-    users.${username} = {
-      home = {
-        stateVersion = "24.05";
-        packages = with pkgs; [
-          dig dmidecode 
-          eza entr tldr bc tree zip
-          pciutils usbutils 
-          cifs-utils samba
-          # cli apps
-          yt-dlp beets spotdl protonvpn-cli_2
-          tesseract ocrmypdf
-          android-tools adb-sync unzip android-tools ffmpeg mpv ventoy
-          # Nix
-          sops  just nixos-anywhere ssh-to-age colmena disko
-        ];
-      };
-      imports =
-        []
-        ++ programs;
-      myHomeManager = {
-        kitty.enable = true; # Enable the kitty module
-        zsh.enable = true; 
-        # bash.enable = false;
-        vim.enable = true; 
-        neovim.enable = true; 
-        ssh.enable = true; 
-        git.enable = true;
-        starship.enable = true;
-      };
+  home-manager.users.${username} = {
+    imports =
+      []
+      ++ programs;
+    myHomeManager = {
+      kitty.enable = true; # Enable the kitty module
+      zsh.enable = true; 
+      bash.enable = false;
+      vim.enable = true; 
+      neovim.enable = true; 
+      ssh.enable = true; 
+      git.enable = true;
+      starship.enable = true;
     };
   };
 }
