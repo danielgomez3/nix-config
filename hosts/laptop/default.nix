0a45bf4f168740bd603cfac8d8cb1be23626dc17
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-14 16:09:23 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -51,10 +51,10 @@ in
 
 
   home-manager.users.${username} = {
-      # myHomeManager = {
-      #   features.gui-apps.enable = true;
-      #   features.cli-apps.enable = true;
-      # };
+      myHomeManager = {
+        gui-apps.enable = true;
+        cli-apps.enable = true;
+      };
       wayland.windowManager.sway = {
         extraConfig = ''
           ## Sleep
