f7af003cebadb0f63b74bed87c2025aff44bfbf2
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-15 10:30:10 -0500

    fixed swaylock and sleep!

--- modules/homeManagerModules/programs/swayidle.nix
+++ modules/homeManagerModules/programs/swayidle.nix
@@ -1,14 +1,15 @@
 { pkgs, lib, ... }: {
 
+  # Inspired by https://github.com/haennes/dotfiles/blob/cfd036748b91636106deb7cbdfdc9ba8cc95c5b8/users/hannses/idle.nix#L2
   services.swayidle = {
     enable = true;
     timeouts = [
       {
-        timeout = 10;
+        timeout = 200;
         command = "${pkgs.swaylock}/bin/swaylock -fF";
       }
       {
-        timeout = 20;
+        timeout = 300;
         command = "${pkgs.systemd}/bin/systemctl suspend";
       }
     ];
