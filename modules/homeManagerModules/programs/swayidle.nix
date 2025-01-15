baa7bc55e9c46cc4510cb4978da4107722879dea
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-15 09:47:26 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/homeManagerModules/programs/swayidle.nix
+++ modules/homeManagerModules/programs/swayidle.nix
@@ -1,57 +1,16 @@
-{
-pkgs,
-config,
-...
-}:
-let
-  swaylock = "${config.programs.swaylock.package}/bin/swaylock";
-  pgrep = "${pkgs.procps}/bin/pgrep";
-  pactl = "${pkgs.pulseaudio}/bin/pactl";
-  sleep = "${pkgs.coreutils}/bin/sleep";
-  hyprctl = "${config.wayland.windowManager.hyprland.package}/bin/hyprctl";
-  swaymsg = "${config.wayland.windowManager.sway.package}/bin/swaymsg";
+{pkgs,lib,...}:{
 
-  isLocked = "${pgrep} -x ${swaylock}";
-  lockTime = 10; 
-
-  # Makes two timeouts: one for when the screen is not locked (lockTime+timeout) and one for when it is.
-  afterLockTimeout =
-    { timeout
-    , command
-    , resumeCommand ? null
-    ,
-    }: [
-      {
-        timeout = lockTime + timeout;
-        inherit command resumeCommand;
-      }
-      {
-        command = "${isLocked} && ${command}";
-        inherit resumeCommand timeout;
-      }
-    ];
-  in
-{
-  services.swayidle = {
+  services.sway = {
     enable = true;
-    systemdTarget = "graphical-session.target";
-    timeouts =
-      # Lock screen
-      [
-        {
-          timeout = lockTime;
-          command = "${sleep} 1 && ${swaylock} --daemonize";
-        }
-      ]
-      ++
-      # Suspend the system
-      [
-        {
-          timeout = lockTime + 10;
-          command = "${isLocked} && systemctl suspend";
-        }
-      ];
+    config = {
+      idle = {
+        enabled = true;
+        timeout = {
+          "10s" = "swaymsg 'output * dpms off'";
+          "20s" = "systemctl suspend";
+        };
+      };
+    };
   };
 
 }
-
