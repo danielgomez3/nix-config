585f758135a6c685a148fbd41898159ebe5fdff6
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-15 09:15:19 -0500

    same

--- modules/homeManagerModules/bundles/desktop-environment.nix
+++ modules/homeManagerModules/bundles/desktop-environment.nix
@@ -2,6 +2,7 @@
 {
   myHomeManager = {
     gui-apps.enable = true; 
+    sway.enable = true;
     swayidle.enable = true;
   }; 
 }
--- modules/homeManagerModules/features/gui-apps.nix
+++ modules/homeManagerModules/features/gui-apps.nix
@@ -2,7 +2,6 @@
 {
 
   myHomeManager = {
-    sway.enable = true;
     wezterm.enable = true;
     kitty.enable = true;
     zathura.enable = true;
