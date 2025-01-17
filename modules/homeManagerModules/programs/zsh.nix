d1148d62c5abb607d1ba41dbfb4f486660115a28
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-17 11:46:41 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/homeManagerModules/programs/zsh.nix
+++ modules/homeManagerModules/programs/zsh.nix
@@ -12,48 +12,6 @@
     history = {
       size = 10000;
     };
-    envExtra = ''
-      #if [[ -o interactive ]]; then
-      #    export GITHUB_TOKEN=$(cat /run/secrets/github_token)
-      #fi
-      export HISTCONTROL=ignoreboth:erasedups
-      # 1 tab autocomplete:
-      #bind 'set show-all-if-ambiguous on'
-      #bind 'set completion-ignore-case on'
-
-      c() { z "$@" && eza --icons --color=always --group-directories-first; }
-      #e() { if [ $# -eq 0 ]; then hx .; else hx "$@"; fi; }
-
-      # NOTE: screenshot
-      _s() {
-        mkdir -p ./.screenshots/
-        read input
-        # Generate filename and path
-        local filename="''${input}.png"
-        local filepath="./.screenshots/''${filename}"
-
-        # Take screenshot using slurp and grim
-        grim -g "$(slurp)" "$filepath"
-
-        # Print Markdown image link to stdout
-        echo "![''${input}]($filepath)"
-      }
-
-      # NOTE: view markdown image link
-
-      screenshot_clipboard() {
-        grim -g "$(slurp)" - | wl-copy
-      }
-      # NOTE: basically this is deprecated i guess
-      _tar(){
-        tar -czhvf ~/Backups/"$(basename "$1")-$(date -I)".tar.gz -C $(dirname "$1") $(basename "$1")
-      }
-      _backup(){
-        mkdir -p ~/Backups/
-        output_name="backup-$(date -I).tar.gz"
-        tar -czhvf ~/Backups/"$output_name" "$@"
-      }
-    '';
     shellAliases = {
       f = "fg";
       j = "jobs";
