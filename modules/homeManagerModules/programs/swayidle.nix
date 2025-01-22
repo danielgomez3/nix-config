dc428b03511091511edb6b76110d33299df70812
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-22 14:17:32 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/homeManagerModules/programs/swayidle.nix
+++ modules/homeManagerModules/programs/swayidle.nix
@@ -2,33 +2,6 @@
 
   services.swayidle = {
     enable = true;
-
-    # timeouts = let
-    #   default = [       
-    #     {
-    #       timeout = 250;
-    #       command = "${pkgs.swaylock}/bin/swaylock -fF";
-    #     }
-    #     {
-    #       timeout = 300;
-    #       command = "${pkgs.systemd}/bin/systemctl suspend";
-    #     }
-    #   ];
-    #   short = [
-    #     {
-    #       timeout = 120;
-    #       command = "${pkgs.swaylock}/bin/swaylock -fF";
-    #     }
-    #     {
-    #       timeout = 220;
-    #       command = "${pkgs.systemd}/bin/systemctl suspend";
-    #     }
-    #   ];
-    # in
-    #   lib.mkMerge [
-    #     (lib.mkIf osConfig.myVars.isHardwareLimited short)
-    #     default
-    #   ];
     timeouts = lib.mkMerge [
       (lib.mkIf osConfig.myVars.isHardwareLimited [
         {
@@ -50,7 +23,6 @@
           command = "${pkgs.systemd}/bin/systemctl suspend";
         }
       ]
-      # Conditional inclusion based on isHardwareLimited
     ];
 
     events = [
