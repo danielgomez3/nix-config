fa76a1b9262e4a9659d62614cc170e91f71e5fca
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-22 12:34:46 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/homeManagerModules/features/hyprland-desktop.nix
+++ modules/homeManagerModules/features/hyprland-desktop.nix
@@ -1,6 +1,6 @@
 {pkgs,lib,...}:
 {
-  # NOTE: This isn't really complete, just an idea. 
+  # NOTE: This isn't complete or working. 
   myHomeManager = {
     hyprland.enable = true;
     # hypridle.enable = true;
--- modules/homeManagerModules/programs/swayidle.nix
+++ modules/homeManagerModules/programs/swayidle.nix
@@ -1,22 +1,38 @@
-{ pkgs, lib, ... }: {
+{ config, pkgs, lib, ... }:
 
+{
+  # Swayidle configuration
   # Inspired by https://github.com/haennes/dotfiles/blob/cfd036748b91636106deb7cbdfdc9ba8cc95c5b8/users/hannses/idle.nix#L2
   services.swayidle = {
     enable = true;
-    timeouts = [
-      {
-        timeout = 250;
-        command = "${pkgs.swaylock}/bin/swaylock -fF";
-      }
-      {
-        timeout = 300;
-        command = "${pkgs.systemd}/bin/systemctl suspend";
-      }
-    ];
+
+    # Conditional timeouts based on isHardwareLimited
+    timeouts = lib.mkIf config.myVars.isHardwareLimited
+      [ # Shorter timeouts for hardware-limited devices
+        {
+          timeout = 100;
+          command = "${pkgs.swaylock}/bin/swaylock -fF";
+        }
+        {
+          timeout = 150;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ]
+      [ # Default timeouts for non-hardware-limited devices
+        {
+          timeout = 250;
+          command = "${pkgs.swaylock}/bin/swaylock -fF";
+        }
+        {
+          timeout = 300;
+          command = "${pkgs.systemd}/bin/systemctl suspend";
+        }
+      ];
+
+    # Events (unchanged)
     events = [{
       event = "lock";
       command = "${pkgs.swaylock}/bin/swaylock -fF";
     }];
   };
-
 }
