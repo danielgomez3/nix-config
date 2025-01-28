0f7e4b5a53c99796618b63e6704561d4e65b8c69
danielgomez3 <danielgomezcoder@gmail.com>
2025-01-28 09:26:24 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- hosts/desktop/disko-config.nix
+++ hosts/desktop/disko-config.nix
@@ -29,4 +29,42 @@
   #     };
   #   };
   # };
+
+  disko.devices = {
+    disk = {
+      my-disk = {
+        device = "/dev/nvme0n1";
+        type = "disk";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              type = "EF00";
+              size = "500M"; # EFI System Partition for both NixOS and Windows
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+              };
+            };
+            nixos-root = {
+              size = "50%"; # Adjust size as needed for NixOS
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+            windows = {
+              size = "50%"; # Reserved space for Windows
+              content = {
+                type = "empty"; # Leave this partition unformatted for Windows
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
 }
