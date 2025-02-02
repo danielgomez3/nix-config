f7b68e7d109ea3cb50072ee6cd58ff6ad4717a48
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-02 15:09:56 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/nixosModules/features/systemd-boot.nix
+++ modules/nixosModules/features/systemd-boot.nix
@@ -1,41 +1,10 @@
 {pkgs,...}:{
-  # Console
-  console =
-  {
-    font = "ter-132n";
-    packages = with pkgs; [ terminus_font ];
-    keyMap = "us";
+
+  myNixOS.silent-boot.enable = true;
+  boot.loader = {
+    systemd-boot.enable = true;
+    efi.canTouchEfiVariables = true;
+    systemd-boot.configurationLimit = 6;
   };
 
-  # TTY
-  fonts.fonts = with pkgs; [ meslo-lgs-nf ];
-  services.kmscon =
-  {
-    enable = true;
-    hwRender = true;
-    extraConfig =
-    ''
-      font-name=MesloLGS NF
-      font-size=14
-    '';
-  };
-
-  # Boot
-  boot =
-  {
-    # Plymouth
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    plymouth.enable = true;
-    kernelParams = [ "quiet" "splash" "rd.systemd.show_status=false" "rd.udev.log_level=3" "udev.log_priority=3" "boot.shell_on_fail" ];
-
-    # Boot Loader
-    loader =
-    {
-      systemd-boot.enable = true;
-      efi.canTouchEfiVariables = true;
-      systemd-boot.configurationLimit = 6;
-      timeout = 0;
-    };
-  };
 }
