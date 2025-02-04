4adb25323ffd4c8e488444a7b50d32db8b2875a1
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-03 19:34:42 -0500

    Successful colmena apply on target(s)! No commit message given

--- modules/nixosModules/programs/borg-backup.nix
+++ modules/nixosModules/programs/borg-backup.nix
@@ -3,12 +3,14 @@ let
   username = config.myVars.username;
 in
 {
+  # FIXME: This requires initial ssh -i access. Make this pure..
   services.borgbackup.jobs."home-${username}" = {
     paths = [
       "/home/${username}/Documents"
     ];
     environment.BORG_RSH = "ssh -i /home/${username}/.ssh/id_ed25519";
-    repo = "ssh://q4mtob1t@q4mtob1t.repo.borgbase.com/./repo";
+    # repo = "ssh://q4mtob1t@q4mtob1t.repo.borgbase.com/./repo";
+    repo = "ssh://q4mtob1t@q4mtob1t.repo.borgbase.com:repo";
     compression = "auto,zstd";
     startAt = "hourly";
     persistentTimer = true;
