e4855e067408e7928e3be52cc7c984a7947fb0c8
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-03 19:05:39 -0500

    CAUTION unreviewed changes. Broken Configuration!

--- modules/nixosModules/programs/borg-backup.nix
+++ modules/nixosModules/programs/borg-backup.nix
@@ -1,6 +1,16 @@
 { config, pkgs, lib, ... }:
+let
+  username = config.myVars.username;
+in
 {
+  services.borgbackup.jobs."home-${username}" = {
+    paths = "/home/${username}";
+    encryption.mode = "none";
+    environment.BORG_RSH = "ssh -i /home/${username}/.ssh/id_ed25519";
+    repo = "ssh://s2iyaw6a@s2iyaw6a.repo.borgbase.com/./repo";
+    compression = "auto,zstd";
+    startAt = "daily";
+  };
 
-  config.myNixOS.borg-backup.enable = true;  
 
 }
