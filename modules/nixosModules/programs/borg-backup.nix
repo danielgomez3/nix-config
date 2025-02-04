5769789222e82a6b37536f9c8c014e549b415cd2
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-03 19:06:10 -0500

    trying for borg

--- modules/nixosModules/programs/borg-backup.nix
+++ modules/nixosModules/programs/borg-backup.nix
@@ -4,7 +4,9 @@ let
 in
 {
   services.borgbackup.jobs."home-${username}" = {
-    paths = "/home/${username}";
+    paths = [
+      "/home/${username}/Documents"
+    ];
     encryption.mode = "none";
     environment.BORG_RSH = "ssh -i /home/${username}/.ssh/id_ed25519";
     repo = "ssh://s2iyaw6a@s2iyaw6a.repo.borgbase.com/./repo";
