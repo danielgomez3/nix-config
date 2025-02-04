2348bb199d4317190ef5fc2898782253f2d53453
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-04 12:54:25 -0500

    Successful colmena apply on target(s)! No commit message given

--- modules/homeManagerModules/features/speech-recognition.nix
+++ modules/homeManagerModules/features/speech-recognition.nix
@@ -1,8 +1,10 @@
-{pkgs,...}:{
+{pkgs, inputs, ...}:{
 
   # Via this command: sox -d $(pocketsphinx soxflags) | pocketsphinx -
   home.packages = with pkgs; [
-    pocketsphinx  # live voice speach recogn.
-    sox  # convert audio files, effects, filters,etc
+    # pocketsphinx  # live voice speach recogn.
+    # sox  # convert audio files, effects, filters,etc
+    # inputs.nerd-dictation.packages.${system}.default
+    inputs.nerd-dictation
   ];
 }
