6798f43b430cff45fe831b6d1dd32fc8955f01ff
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-03 20:07:03 -0500

    Successful colmena apply on target(s)! No commit message given

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -19,7 +19,7 @@ in
     bundles.base-system.enable = true;
     netboot.enable = true;
     hydra.enable = true;
-    borg-backup.enable = false;
+    borg-backup.enable = true;
   };
   home-manager.users.${username}.myHomeManager = {
     cli-apps.enable = true;
