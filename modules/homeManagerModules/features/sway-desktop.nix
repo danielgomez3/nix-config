4f4944483b841dd2ff1f62eab0b2289f7c924b6f
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-04 11:43:34 -0500

    Successful colmena apply on target(s)! No commit message given

--- modules/homeManagerModules/features/sway-desktop.nix
+++ modules/homeManagerModules/features/sway-desktop.nix
@@ -3,7 +3,6 @@
   myHomeManager = {
     sway.enable = true;
     swayidle.enable = true;
-    # mako.enable = true;  # Wayland notifications
     dunst.enable = true;  # Wayland notifications
     wayland-pipewire-idle-inhibit.enable = true;
   }; 
