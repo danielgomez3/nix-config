2a5561e4a84019866227c916aea434c000315001
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-03 23:51:35 -0500

    Successful colmena apply on target(s)! No commit message given

--- modules/nixosModules/programs/borg-backup.nix
+++ modules/nixosModules/programs/borg-backup.nix
@@ -8,12 +8,8 @@ in
     paths = [
       "/home/${username}/Documents"
     ];
-      environment = {
-        BORG_RSH = "ssh -i /root/.ssh/id_ed25519";  
-        BORG_REPO_FILE = config.sops.secrets."borgbase/repo".path;
-      };
-    # repo = "ssh://q4mtob1t@q4mtob1t.repo.borgbase.com/./repo";
-    repo = "@BORG_REPO@";  # Placeholder to be replaced at runtime
+    environment.BORG_RSH = "ssh -i /root/.ssh/id_ed25519";  # FIXME use pure sops nix path
+    repo = "ssh://q4mtob1t@q4mtob1t.repo.borgbase.com/./repo";
     compression = "auto,zstd";
     startAt = "daily";
     persistentTimer = true;
