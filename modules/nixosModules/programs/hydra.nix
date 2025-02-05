6d7b37a333e75ea43641416c800afa4da006cec5
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-05 14:35:26 -0500

    Successful colmena apply on target(s)! No commit message given

--- modules/nixosModules/programs/hydra.nix
+++ modules/nixosModules/programs/hydra.nix
@@ -9,7 +9,7 @@
     hydraURL = "http://localhost:3000"; # externally visible URL
     notificationSender = "danielgomezcoder@gmail.com"; # e-mail of hydra service
     # a standalone hydra will require you to unset the buildMachinesFiles list to avoid using a nonexistant /etc/nix/machines
-    buildMachinesFiles = [];
+    buildMachinesFiles = [ "/etc/nix/machines" ];
     # you will probably also want, otherwise *everything* will be built from scratch
     useSubstitutes = true;
     # extraConfig = ''
