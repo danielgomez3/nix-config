b9afffd1d78b95184f3bb6af0413fbe4b3221072
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-04 21:15:28 -0500

    trying for colmena on hive

--- modules/nixosModules/programs/hydra.nix
+++ modules/nixosModules/programs/hydra.nix
@@ -5,7 +5,7 @@
   });
 
   services.hydra = {
-    enable = false;
+    enable = true;
     hydraURL = "http://localhost:3000"; # externally visible URL
     notificationSender = "danielgomezcoder@gmail.com"; # e-mail of hydra service
     # a standalone hydra will require you to unset the buildMachinesFiles list to avoid using a nonexistant /etc/nix/machines
