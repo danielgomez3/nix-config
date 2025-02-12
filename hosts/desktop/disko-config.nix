4b5302b1d46d4f9c7d11c961af316691c2d1de48
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-11 19:55:12 -0500

    changed desktop disko, hopefully not busted

--- hosts/desktop/disko-config.nix
+++ hosts/desktop/disko-config.nix
@@ -1,35 +1,4 @@
 {
-  disko.devices = {
-    disk = {
-      my-disk = {
-        device = "/dev/nvme0n1";
-        type = "disk";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              type = "EF00";
-              size = "500M";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-              };
-            };
-            root = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
-    };
-  };
-
   # disko.devices = {
   #   disk = {
   #     my-disk = {
@@ -40,28 +9,19 @@
   #         partitions = {
   #           ESP = {
   #             type = "EF00";
-  #             size = "500M"; # EFI System Partition for both NixOS and Windows
+  #             size = "500M";
   #             content = {
   #               type = "filesystem";
   #               format = "vfat";
   #               mountpoint = "/boot";
-  #               mountOptions = [ "fmask=0077" "dmask=0077" ];
   #             };
   #           };
-  #           nixos-root = {
-  #             size = "50%"; # Adjust size as needed for NixOS
+  #           root = {
+  #             size = "100%";
   #             content = {
   #               type = "filesystem";
   #               format = "ext4";
   #               mountpoint = "/";
-  #             };
-  #           };
-  #           windows = {
-  #             size = "50%"; # Reserved space for Windows
-  #             content = {
-  #               type = "filesystem"; # Leave this partition unformatted for Windows
-  #               format = "ntfs";
-  #               mountpoint = null; # Windows will use this; NixOS won't mount it
   #             };
   #           };
   #         };
@@ -69,5 +29,47 @@
   #     };
   #   };
   # };
+
+  disko.devices = {
+    disk = {
+      "my-disk" = {
+        # suffix is to prevent disk name collisions
+        type = "disk";
+        # Set the following in flake.nix for each maschine:
+        # device = <uuid>;
+        content = {
+          type = "gpt";
+          partitions = {
+            "boot" = {
+              size = "1M";
+              type = "EF02"; # for grub MBR
+              priority = 1;
+            };
+            "ESP" = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "nofail" ];
+              };
+            };
+            "root" = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                # format = "btrfs";
+                # format = "bcachefs";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+
 
 }
