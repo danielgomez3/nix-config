72a5c1396e4600120b6a6f385b9dfaee195cf06d
Daniel Gomez <danielgomezcoder@gmail.com>
2025-02-13 00:45:02 -0500

    trying to apply desktop to self

--- hosts/desktop/disko-config.nix
+++ hosts/desktop/disko-config.nix
@@ -30,46 +30,46 @@
   #   };
   # };
 
-  disko.devices = {
-    disk = {
-      "my-disk" = {
-        # suffix is to prevent disk name collisions
-        type = "disk";
-        # Set the following in flake.nix for each maschine:
-        # device = <uuid>;
-        content = {
-          type = "gpt";
-          partitions = {
-            "boot" = {
-              size = "1M";
-              type = "EF02"; # for grub MBR
-              priority = 1;
-            };
-            "ESP" = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [ "nofail" ];
-              };
-            };
-            "root" = {
-              size = "100%";
-              content = {
-                type = "filesystem";
-                format = "ext4";
-                # format = "btrfs";
-                # format = "bcachefs";
-                mountpoint = "/";
-              };
-            };
-          };
-        };
-      };
-    };
-  };
+  # disko.devices = {
+  #   disk = {
+  #     "my-disk" = {
+  #       # suffix is to prevent disk name collisions
+  #       type = "disk";
+  #       # Set the following in flake.nix for each maschine:
+  #       # device = <uuid>;
+  #       content = {
+  #         type = "gpt";
+  #         partitions = {
+  #           "boot" = {
+  #             size = "1M";
+  #             type = "EF02"; # for grub MBR
+  #             priority = 1;
+  #           };
+  #           "ESP" = {
+  #             size = "512M";
+  #             type = "EF00";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "vfat";
+  #               mountpoint = "/boot";
+  #               mountOptions = [ "nofail" ];
+  #             };
+  #           };
+  #           "root" = {
+  #             size = "100%";
+  #             content = {
+  #               type = "filesystem";
+  #               format = "ext4";
+  #               # format = "btrfs";
+  #               # format = "bcachefs";
+  #               mountpoint = "/";
+  #             };
+  #           };
+  #         };
+  #       };
+  #     };
+  #   };
+  # };
 
 
 }
