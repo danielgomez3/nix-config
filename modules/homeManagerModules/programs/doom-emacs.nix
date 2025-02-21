66c201113a21791361d939e6803d72eeea11505d
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-21 14:35:48 -0500

    Successful colmena apply on target(s)! No commit message given

--- modules/homeManagerModules/programs/doom-emacs.nix
+++ modules/homeManagerModules/programs/doom-emacs.nix
@@ -10,15 +10,17 @@
     rsync
     openssh
     scrot
+    emacsPackages.lsp-haskell
+
     # hunspell 
     # aspell  # To do actuall spell correction with z =
   ];
   programs.doom-emacs = {
     enable = true;
     doomDir = "${self.outPath}/extra/doom.d"; # Directory containing your config.el, init. and packages.el files
-    # extraPackages = epkgs: [
-    #   pkgs.emacsPackages.org-attach-screenshot
-    # ];
+    extraPackages = pkgs: [
+      # pkgs.emacsPackages.lsp-haskell
+    ];
   };
 
 }
