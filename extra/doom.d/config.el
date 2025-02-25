eeb83051394e59e69086c75e84dc95ae6e88cf8b
danielgomez3 <danielgomezcoder@gmail.com>
2025-02-24 19:59:06 -0500

    Successful colmena apply on target(s)! No commit message given

--- extra/doom.d/config.el
+++ extra/doom.d/config.el
@@ -12,3 +12,16 @@
 ;; Org mode stuff
 (add-hook 'org-mode-hook #'org-modern-mode)
 (add-hook 'org-agenda-finalize-hook #'org-modern-agenda)
+
+;; Org-download (for screenshots?)
+(if (string= system-wm-type "wayland")
+  (setq org-download-screenshot-method "grim -g \"$(slurp)\" %s")
+  (setq org-download-screenshot-method "flameshot gui -p %s")
+)
+(require 'org-download)
+(map! :leader
+      :desc "Insert a screenshot"
+      "i s" 'org-download-screenshot
+      :desc "Insert image from clipboard"
+      "i p" 'org-download-clipboard
+      "i P" 'org-download-clipboard-basename)
