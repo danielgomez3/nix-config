29ba90af43ba90ec2fb07270d4815db346f2a66a
danielgomez3 <danielgomezcoder@gmail.com>
2025-03-03 12:00:12 -0500

    fixed zsh

--- modules/homeManagerModules/programs/zsh.nix
+++ modules/homeManagerModules/programs/zsh.nix
@@ -15,15 +15,16 @@
     shellAliases = {
       # f = "fg";
       # j = "jobs";
+      l = "ls -l";
       ls = "eza --icons --color=always --group-directories-first";
       la = "eza -a --icons --color=always --group-directories-first";
-      # lt = "eza --icons --color=always --tree --level 2 --group-directories-first";
-      # lta = "eza -a --icons --color=always --tree --level 2 --group-directories-first";
+      lt = "eza --icons --color=always --tree --level 2 --group-directories-first";
+      lta = "eza -a --icons --color=always --tree --level 2 --group-directories-first";
       # grep = "grep --color=always -IrnE --exclude-dir='.*'";
       # less = "less -FR";
-      productivity = "cd ~/Documents/productivity/ && hx todo.md credentials.md";
+      productivity = "cd ~/Documents/productivity/files && hx todo.md credentials.md";
       conf = "cd ~/Projects/repos-personal/flakes/flake/ && hx modules/coding.nix modules/all.nix";
-      notes = "cd ~/Documents/notes && hx .";
+      notes = "cd ~/Documents/notes/files && hx .";
       zrf = "zellij run floating";
       # send_desktop_downloads_to_server = "${pkgs.rsync}/bin/rsync --remove-source-files -avz desktop:~/Downloads/* server:~/Downloads/";
       # send_desktop_downloads_to_server_cwd = "${pkgs.rsync}/bin/rsync --remove-source-files -avz desktop:~/Downloads/* server:~/Downloads/";
