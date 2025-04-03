078304e4a7d2b5746d76d0152b955888348d2a83
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-03 14:54:18 -0400

    Successful system build and applly!

--- modules/nixosModules/programs/borg-backup.nix
+++ modules/nixosModules/programs/borg-backup.nix
@@ -33,7 +33,7 @@ in
     # BORG_RSH = "ssh -i ${config.age.secrets.borg-ssh-key.path}";
     # NOTE: Stable, working:
     # environment.BORG_RSH = "ssh -i /root/.ssh/id_ed25519";  
-    BORG_RSH = "ssh -i ${config.sops.secrets."private_ssh_keys/server_root".path}";
+    environment.BORG_RSH = "ssh -i ${config.sops.secrets."private_ssh_keys/server_root".path}";
     repo = "ssh://q4mtob1t@q4mtob1t.repo.borgbase.com/./repo";
     compression = "auto,zstd";
     startAt = "daily";
