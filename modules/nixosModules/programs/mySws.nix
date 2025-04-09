7f2455cacd93bc1408028d8d5245100d0a83c0a8
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-09 18:19:00 -0400

    Successful system build and/or apply to targets: server

--- modules/nixosModules/programs/mySws.nix
+++ modules/nixosModules/programs/mySws.nix
@@ -1,5 +1,8 @@
+# https://nixos.wiki/wiki/Static_Web_Server
+
 {config,pkgs,lib,...}:{
 
+  # NOTE: By default, this will start SWS on [::]:8787
   services.static-web-server = {
     enable = true;
     root = "/home/${config.myVars.username}/my-http-server-root-dir";
