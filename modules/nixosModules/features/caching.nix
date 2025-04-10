dd9adcaa81f2a6fd5b0248531b90d91ffd160bab
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-10 11:47:42 -0400

    Successful system build and/or apply to targets: desktop

--- modules/nixosModules/features/caching.nix
+++ modules/nixosModules/features/caching.nix
@@ -4,6 +4,9 @@
     cachix
   ];
 
+  # For some reason couldn't use cachix unless I was a 'trust-user'
+  nix.settings.trusted-users = [ "danielgomez3" ];
+
   nix.settings.substituters = [
     "https://cachix.cachix.org"
   ];
