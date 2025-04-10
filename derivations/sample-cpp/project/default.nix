e80f773b0562062f972d0ac3f09ca9a185b89a3c
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-09 21:15:00 -0400

    System build failed, commit broken!

---
+++ derivations/sample-cpp/project/default.nix
@@ -0,0 +1,7 @@
+{ pkgs ? import <nixpkgs> {} }:{
+
+  pkgs.stdenv.mkDerivation {
+    
+  }
+  
+}
--- modules/nixosModules/programs/mySws.nix
+++ modules/nixosModules/programs/mySws.nix
@@ -1,26 +1,23 @@
 # https://nixos.wiki/wiki/Static_Web_Server
 
-{config,pkgs,lib,...}:{
+{self,config,pkgs,lib,...}:
+let
+  myWebsite = pkgs.callPackage "${self.outPath}/derivation/mySws" {};
+in
+{
 
   # NOTE: We need to create this dir first, or make sure it exists:
   systemd.tmpfiles.rules = [
     "d /var/www 0755 staticweb staticweb -"
   ];
 
-  # DELETEME: not needed
-  # users.users.staticweb.extraGroups = [ "staticweb" ];
-  # users.groups.staticweb = {};
-  # users.users.staticweb = {
-  #   isSystemUser = true;
-  #   group = "staticweb";
-  #   hashedPasswordFile = config.sops.secrets.user_password.path;  
-  # };
 
   # NOTE: By default, this will start SWS on [::]:8787
   # sudo systemctl status static-web-server.service
   services.static-web-server = {
     enable = true;
-    root = "/var/www";
+    # root = "/var/www";  # FIXME: let's do the nix store instead
+    root = "${myWebsite}";  
   };
 
 }
