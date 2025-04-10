483a3e0e0133adbb54fa9a899d13127c2d88ecfe
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-10 13:31:03 -0400

    Successful system build and/or apply to targets:

--- modules/nixosModules/features/caching.nix
+++ modules/nixosModules/features/caching.nix
@@ -11,4 +11,9 @@
     "https://cachix.cachix.org"
   ];
 
+  nix.settings.trusted-public-keys = [
+    "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY= nix-on-droid.cachix.org-1:56snoMJTXmDRC1Ei24CmKoUqvHJ9XCp+nidK7qkMQrU="
+  ];
+
+
 }
