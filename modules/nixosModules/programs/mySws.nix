229c1822b5ac8c7dd6c4dad35fa96be806fea948
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-09 20:31:34 -0400

    System build failed, commit broken!

--- modules/nixosModules/programs/mySws.nix
+++ modules/nixosModules/programs/mySws.nix
@@ -6,14 +6,15 @@
   systemd.tmpfiles.rules = [
     "d /var/www 0755 staticweb staticweb -"
   ];
-  # NOTE: And create a user with limited permissions:
+
+  # DELETEME: not needed
   # users.users.staticweb.extraGroups = [ "staticweb" ];
-  users.groups.staticweb = {};
-  users.users.staticweb = {
-    isSystemUser = true;
-    group = "staticweb";
-    hashedPasswordFile = config.sops.secrets.user_password.path;  
-  };
+  # users.groups.staticweb = {};
+  # users.users.staticweb = {
+  #   isSystemUser = true;
+  #   group = "staticweb";
+  #   hashedPasswordFile = config.sops.secrets.user_password.path;  
+  # };
 
   # NOTE: By default, this will start SWS on [::]:8787
   # sudo systemctl status static-web-server.service
