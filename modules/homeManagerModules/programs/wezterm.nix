f1a9346cc408864b11a691f5681ea63266cbf5c2
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-28 20:10:37 -0400

    Successful system build and/or apply to targets: desktop

--- modules/homeManagerModules/programs/wezterm.nix
+++ modules/homeManagerModules/programs/wezterm.nix
@@ -12,16 +12,14 @@
 
         window_decorations = "NONE",
         audible_bell = "Disabled",
-
-        -- HACKME: see if can remove in future NixOS version or patch
-        -- front_end = "WebGpu",  
         hide_tab_bar_if_only_one_tab = true,
         warn_about_missing_glyphs = false,  -- Disables missing glyph warnings
-        font = wezterm.font_with_fallback({'Firacode Nerd Font Mono','Droid Sans Fallback'})
+        font = wezterm.font_with_fallback({'Firacode Nerd Font Mono','Droid Sans Fallback'}),
 
         keys = {
           -- Disable Alt+Enter (fullscreen toggle)
           { key = "Enter", mods = "ALT", action = wezterm.action.DisableDefaultAssignment },
+          
         },
 
       }
