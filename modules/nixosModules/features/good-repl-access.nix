874bf40cdb00c0516d77806516d81bed009d69ea
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-29 18:22:31 -0400

    Successful system build and/or apply to targets: server

--- modules/nixosModules/features/good-repl-access.nix
+++ modules/nixosModules/features/good-repl-access.nix
@@ -1,3 +1,6 @@
+# https://nixos.wiki/wiki/Flakes
+# This allows for:
+# This will launch a 'nix repl' with access to nixpkgs, lib, and the flake options in a split of a second, instead of manually loading it in or doing some acrobatics in the justfile or with args.
 {inputs,...}:{
   nix.nixPath =
     let
