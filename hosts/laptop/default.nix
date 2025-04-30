6c6d3f929f9af0b340f1df45955950c141d3720b
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-30 07:24:34 -0400

    Successful system build and/or apply to targets: laptop

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -7,7 +7,7 @@ let
   username = config.myVars.username;
 in
 {
-  myVars.username = "${inputs.config.sops.secrets."username/default"}";  # Specific username for this machine
+  myVars.username = "${config.sops.secrets."username/default".path}";  # Specific username for this machine
   myVars.hostname = "laptop";  # Specific hostname for this machine
   myVars.isHardwareLimited = true;
   myVars.isSyncthingClient = true;
