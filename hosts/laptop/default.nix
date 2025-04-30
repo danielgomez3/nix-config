52ad333be5543cc4b4f8c906c851bc6df2774de4
danielgomez3 <danielgomezcoder@gmail.com>
2025-04-30 08:05:15 -0400

    System build failed, commit broken!

--- hosts/laptop/default.nix
+++ hosts/laptop/default.nix
@@ -7,7 +7,7 @@ let
   username = config.myVars.username;
 in
 {
-  myVars.username = toString config.sops.secrets."username/default".path;
+  myVars.username = "daniel";
   myVars.hostname = "laptop";  # Specific hostname for this machine
   myVars.isHardwareLimited = true;
   myVars.isSyncthingClient = true;
