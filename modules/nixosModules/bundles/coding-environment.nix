1092ff65e747d1b5a4a94a7ecbcf168eedb43a02
danielgomez3 <danielgomezcoder@gmail.com>
2025-05-01 07:13:01 -0400

    System build failed, commit broken!

--- modules/nixosModules/bundles/coding-environment.nix
+++ modules/nixosModules/bundles/coding-environment.nix
@@ -5,17 +5,17 @@ in
 {
     environment.variables.EDITOR = "${pkgsUnstable.helix}";
     # TODO: make sql crap as HM module sql.nix instead
-    services.mysql = {
-      enable = true;
-      package = pkgs.mariadb;
-      dataDir = "/var/lib/mysql";
-      ensureUsers = [
-        {
-          name = "root";
-          ensurePermissions = {
-            "*.*" = "ALL PRIVILEGES";
-          };
-        }
-      ];
-    };
+    # services.mysql = {
+    #   enable = true;
+    #   package = pkgs.mariadb;
+    #   dataDir = "/var/lib/mysql";
+    #   ensureUsers = [
+    #     {
+    #       name = "root";
+    #       ensurePermissions = {
+    #         "*.*" = "ALL PRIVILEGES";
+    #       };
+    #     }
+    #   ];
+    # };
 }
