de88e5eb9cfb999198434e2a8016fa8caae9066e
danielgomez3 <danielgomezcoder@gmail.com>
2025-05-01 16:22:54 -0400

    Successful system build and/or apply to targets: laptop

--- modules/homeManagerModules/programs/git.nix
+++ modules/homeManagerModules/programs/git.nix
@@ -8,9 +8,14 @@ config,
     enable = true;
     userName = "danielgomez3";
     userEmail = "danielgomezcoder@gmail.com";  # FIXME: use sops nix, but doesn't seem to work:     defaults.email = "${toString config.sops.secrets.email}";
+    signing = {
+      key = "6D55D3035B3FE40402D8E8AE480228117EAA56B7";
+      signByDefault = true;
+    };
 
     extraConfig = {
       credential.helper = "store";
+      gpg.format = "ssh";
     };
   };
 
