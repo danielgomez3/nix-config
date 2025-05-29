2909376a12b82a0fbd6cd1f54deb86a1eaeedcde
danielgomez3 <danielgomezcoder@gmail.com>
2025-05-29 10:55:51 -0400

    ollama

--- hosts/server/default.nix
+++ hosts/server/default.nix
@@ -1,15 +1,17 @@
-
 # server.nix
 # NOTE: This contains all common features I want only my server to have!
-
-{ config, pkgs, lib, inputs, host, ... }:
-
-let 
-  username = config.myVars.username;
-in
 {
-  myVars.username = "danielgomez3";  # Specific username for this machine
-  myVars.hostname = "server";  # Specific hostname for this machine
+  config,
+  pkgs,
+  lib,
+  inputs,
+  host,
+  ...
+}: let
+  username = config.myVars.username;
+in {
+  myVars.username = "danielgomez3"; # Specific username for this machine
+  myVars.hostname = "server"; # Specific hostname for this machine
   myVars.isSyncthingServer = true;
 
   users.users.${username} = {
@@ -27,26 +29,25 @@ in
     password-manager.enable = true;
     remoteDeployment-nix-on-droid.enable = true;
     mySws.enable = true;
-    macos-emulation.enable = true;    
+    macos-emulation.enable = false;
     docker.enable = true;
+    ollama.enable = true;
   };
 
   home-manager.users.${username}.myHomeManager = {
     bundles.desktop-environment.enable = true;
     bundles.coding-environment.enable = true;
-    cli-apps.enable = true;  # NOTE: Has to be enabled here, we don't inherit it anywhere in bundles.
+    cli-apps.enable = true; # NOTE: Has to be enabled here, we don't inherit it anywhere in bundles.
   };
   environment = {
     # sessionVariables = {
-    #   GITHUB_TOKEN = config.sops.secrets.github_token.path;  
+    #   GITHUB_TOKEN = config.sops.secrets.github_token.path;
     #   GITHUB_TOKEN = "$(cat ${config.sops.secrets.github_token.path})";
     # };
     systemPackages = with pkgs; [
-      kitty  # Make SSHing into this pretty.
+      kitty # Make SSHing into this pretty.
     ];
   };
-
-
 
   security.acme = {
     defaults.email = "${toString config.sops.secrets.email}";
@@ -60,8 +61,6 @@ in
     # DELETME:
     syncthing = {
       guiAddress = "0.0.0.0:8384";
-    };   
+    };
   };
-
-
 }
