5ec2eeae6cace02c0352b9af9947fc996225489f
danielgomez3 <danielgomezcoder@gmail.com>
2025-05-29 11:03:26 -0400

    Successful system build and/or apply to targets: server

--- modules/nixosModules/programs/ollama.nix
+++ modules/nixosModules/programs/ollama.nix
@@ -1,4 +1,8 @@
 {...}: {
+  # environment.systemPackages = [pkgs.chatbox];
+  services.open-webui = {
+    enable = true;
+  };
   services.ollama = {
     enable = true;
     loadModels = ["deepseek-r1:70b"];
