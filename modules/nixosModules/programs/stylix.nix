8f7c9e66ee20967009b3cc514b5a68be90d8b400
danielgomez3 <danielgomezcoder@gmail.com>
2025-06-13 10:36:53 -0400

    fg

--- modules/nixosModules/programs/stylix.nix
+++ modules/nixosModules/programs/stylix.nix
@@ -16,32 +16,11 @@
     image = "${self.outPath}/modules/nixosModules/additional/wallpapers/nord-purple.jpg";
     base16Scheme = "${pkgs.base16-schemes}/share/themes/monokai.yaml";
 
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox.yaml";
-    # cursor = {
-    #   package = pkgs.bibata-cursors;
-    #   name = "Bibata-Modern-Ice";
-    #   # size = 50;
-    # };
-    # targets.nixvim.enable = false;
-    # fonts = {
-    #   monospace = {
-    #     package = pkgs.nerdfonts.override {fonts = ["JetBrainsMono"];};
-    #     name = "JetBrainsMono Nerd Font Mono";
-    #   };
-    #   sansSerif = {
-    #     package = pkgs.dejavu_fonts;
-    #     name = "DejaVu Sans";
-    #   };
-    #   serif = {
-    #     package = pkgs.dejavu_fonts;
-    #     name = "DejaVu Serif";
-    #   };
-    # };
-    # targets = {
-    #   helix.enable = true;
-    #   sway.enable = true;
-    #   swaylock.enable = true;
-    #   wezterm.enable = true;
-    # };
+    targets = {
+      helix.enable = true;
+      sway.enable = true;
+      swaylock.enable = true;
+      wezterm.enable = true;
+    };
   };
 }
