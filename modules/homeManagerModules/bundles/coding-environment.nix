6356e3f5208d5f1781f712c0b5eb8d62b910545a
danielgomez3 <danielgomezcoder@gmail.com>
2025-07-07 09:16:45 -0400

    Successful system build and/or apply to targets: server

--- modules/homeManagerModules/bundles/coding-environment.nix
+++ modules/homeManagerModules/bundles/coding-environment.nix
@@ -38,6 +38,7 @@
     krabby
     cowsay
     figlet
+    slides
     #hollywood
     # coding
     nixpkgs-fmt
--- modules/homeManagerModules/programs/zsh.nix
+++ modules/homeManagerModules/programs/zsh.nix
@@ -36,7 +36,7 @@
       enable = true;
       plugins = [
         {name = "hlissner/zsh-autopair";}
-        {name = "marlonrichert/zsh-hist";}
+        # {name = "marlonrichert/zsh-hist";}
         # {name = "spaceship-prompt/spaceship-prompt";}
       ];
     };
