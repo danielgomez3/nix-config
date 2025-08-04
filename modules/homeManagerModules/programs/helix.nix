73ed87dc7d66ad6a2eff1a2d1b8d4a0c83c2b6a1
danielgomez3 <danielgomezcoder@gmail.com>
2025-08-04 18:52:48 -0400

    

--- modules/homeManagerModules/programs/helix.nix
+++ modules/homeManagerModules/programs/helix.nix
@@ -166,6 +166,17 @@
     language-server = {
       scls = {
         command = lib.getExe pkgs.simple-completion-language-server;
+        config = {
+          max_completion_items = 100; # set max completion results len for each group: words, snippets, unicode-input
+          feature_words = true; # enable completion by word
+          feature_snippets = true; # enable snippets
+          snippets_first = true; # completions will return before snippets by default
+          snippets_inline_by_word_tail = true; # suggest snippets by WORD tail, for example text `xsq|` become `x^2|` when snippet `sq` has body `^2`
+          feature_unicode_input = true; # enable "unicode input"
+          feature_paths = true; # enable path completion
+          citation = true;
+          feature_citations = true; # enable citation completion (only on `citation` feature enabled)
+        };
       };
 
       basedpyright = {
