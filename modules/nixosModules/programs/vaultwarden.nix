dfa03d5e09902a4d27d228099b64148e5ba4ef49
danielgomez3 <danielgomezcoder@gmail.com>
2025-08-11 17:29:14 -0400

    Successful system build and/or apply to targets: server

--- modules/nixosModules/programs/vaultwarden.nix
+++ modules/nixosModules/programs/vaultwarden.nix
@@ -4,14 +4,14 @@
   ...
 }: {
   services.vaultwarden = {
-    enable = true;
+    enable = false;
     backupDir = "/var/lib/vaultwarden/backup";
     # in order to avoid having  ADMIN_TOKEN in the nix store it can be also set with the help of an environment file
     # be aware that this file must be created by hand (or via secrets management like sops)
     environmentFile = config.sops.secrets.vaultwarden-env.path;
     config = {
       # Refer to https://github.com/dani-garcia/vaultwarden/blob/main/.env.template
-      DOMAIN = "https://bitwarden.example.com";
+      DOMAIN = "danielgomezcoder.org";
       SIGNUPS_ALLOWED = false;
 
       ROCKET_ADDRESS = "127.0.0.1";
@@ -22,12 +22,12 @@
       # thus without transport encryption.
       # If you use an external mail server, follow:
       #   https://github.com/dani-garcia/vaultwarden/wiki/SMTP-configuration
-      SMTP_HOST = "127.0.0.1";
-      SMTP_PORT = 25;
-      SMTP_SSL = false;
+      SMTP_HOST = "smtp.gmail.com";
+      SMTP_PORT = 465;
+      SMTP_SSL = true;
 
-      SMTP_FROM = "admin@bitwarden.example.com";
-      SMTP_FROM_NAME = "example.com Bitwarden server";
+      # SMTP_FROM = "admin@bitwarden.example.com";
+      SMTP_FROM_NAME = "My Password Manager";
     };
   };
 }
