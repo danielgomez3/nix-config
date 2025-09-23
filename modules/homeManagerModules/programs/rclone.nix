586294441da39fd083f98ca426f342e816043229
danielgomez3 <danielgomezcoder@gmail.com>
2025-09-23 10:23:25 -0400

    google

--- modules/homeManagerModules/programs/rclone.nix
+++ modules/homeManagerModules/programs/rclone.nix
@@ -4,13 +4,16 @@
   config,
   ...
 }: {
-  programs.rclone.remotes.gdrive = {
-    config = {
-      type = "drive";
-      # drive_type = "business";
-    };
-    secrets = {
-      token = config.age.secrets."google_drive/token".path;
+  programs.rclone = {
+    enable = true;
+    remotes.gdrive = {
+      config = {
+        type = "drive";
+        # drive_type = "business";
+      };
+      secrets = {
+        token = config.age.secrets."google_drive/token".path;
+      };
     };
   };
 }
