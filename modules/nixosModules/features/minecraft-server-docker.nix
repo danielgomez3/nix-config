bd90983b010770e3875b6cf01a27745bb3b4971d
danielgomez3 <danielgomezcoder@gmail.com>
2025-10-30 16:17:03 -0400

    fixing rlcraft, you can grab stuff now

--- modules/nixosModules/features/minecraft-server-docker.nix
+++ modules/nixosModules/features/minecraft-server-docker.nix
@@ -1,11 +1,25 @@
 # minecraft-server-docker.nix
-{config, ...}: {
+{
+  config,
+  pkgs,
+  ...
+}: {
+  environment.systemPackages = [pkgs.docker-compose];
   virtualisation.oci-containers.backend = "docker";
   virtualisation.oci-containers.containers.rlcraft = {
     image = "itzg/minecraft-server:java8";
     autoStart = true;
 
+    # FIXME: This worked for now, but may be impure!
+    # I think it's becuase these already existed?
     volumes = [
+      "/var/lib/minecraft/rlcraft/data:/data"
+      "/var/lib/minecraft/rlcraft/mods:/mods"
+      "/var/lib/minecraft/rlcraft/config:/config"
+      "/var/lib/minecraft/rlcraft/plugins:/plugins"
+
+      # Mount your existing world (optional - if you want to restore from backup)
+      # "/path/to/your/existing/world:/data/world:ro"
     ];
 
     environment = {
@@ -23,6 +37,7 @@
       MEMORY = "8G";
       OPS = "LittleBee_\njodango2814\nWorthyDragoon94";
       RCON_CMDS_STARTUP = "gamerule keepInventory true";
+      # WORLD = "http://danielgomezcoder.org/minecraft/world-saves/rlcraft/rlcraft-cdb_2025-10-30/World.zip"; # XXX: this is impure! Server depends on itself to already exist! Invest in blob storage for now or smth idk
     };
 
     ports = ["25565:25565"];
